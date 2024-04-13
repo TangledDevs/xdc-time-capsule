c93a7b6a26d2c361e3dcc70697e11db203e33216
VijayKumarReddyTalakola <109469574+VijayKumarReddyTalakola@users.noreply.github.com>
2024-04-13 13:13:43 +0530

    Add Home Section

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,9 @@
-import { Button } from "./components/ui/button";
+import Home from "./pages/Home";
 
 function App() {
   return (
     <>
-      <Button>Click Me</Button>
+      <Home />
     </>
   );
 }
